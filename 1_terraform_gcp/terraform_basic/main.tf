d85e0edd4f43aff5708c70f40a4cd894fe8eed52
刘瑞珠 <liuruizhu@MacBook-Pro.local>
2024-04-12 12:14:29 +1000

    finished module 3

---
+++ 1_terraform_gcp/terraform_basic/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "dtc-de-419107"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "dtc-de-419107-terra-bucket"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  } 
+
+  force_destroy = true
+}
