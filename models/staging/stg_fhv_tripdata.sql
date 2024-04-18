4af7bc27729ae0f72117d80eeb13344f592506d1
RUIZHU LIU <liurzzzzz@gmail.com>
2024-04-18 12:44:57 +0000

    homework for module4

---
+++ models/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,27 @@
+{{ config(materialized='view') }}
+
+with tripdata as 
+(
+  select *
+  from {{ source('staging','fhv_2019_trips') }}
+)
+
+select
+    dispatching_base_num,
+
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- identifiers
+    {{ dbt.safe_cast("PUlocationID", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("DOlocationID", api.Column.translate_type("integer")) }} as dropoff_locationid,
+
+    SR_Flag,
+    Affiliated_base_number as affiliated_base_number
+from tripdata
+
+{% if var('is_test_run', default=true) %}
+    limit 100
+{% endif %}
+
